17d8ba1cbe9d5037242a26fe6262a0b11aceaf27
Shubham Thakkar <134299058+SHUBHAM-THAKKAR07@users.noreply.github.com>
2026-07-25 23:35:30 +0530

    Refactor CORS options to use an array of origins

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,24 +1,14 @@
 import cors from 'cors';
-import env from './env';
 
 export const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) => {
-    // If CORS_ORIGIN is '*', allow all origins
-    if (env.CORS_ORIGIN === '*') {
-      callback(null, true);
-      return;
-    }
-
-    const allowedOrigins = env.CORS_ORIGIN.split(',').map((o) => o.trim());
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'https://github.io', // Allow your production live website
+    'http://localhost:5173',               // Keep local Vite development working
+    'http://localhost:3000'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
 };
 
 export const corsMiddleware = cors(corsOptions);
