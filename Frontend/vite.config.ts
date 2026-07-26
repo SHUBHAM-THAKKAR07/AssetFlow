d0cda603d69eaf0c6b70c6577cdc2d4e8d4b5f0a
Shubham Thakkar <134299058+SHUBHAM-THAKKAR07@users.noreply.github.com>
2026-07-26 13:48:33 +0530

    update vite.config and build vercel json file

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,7 +8,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/AssetFlow/', // 👈 This fixes the 404 errors for GitHub Pages assets
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
