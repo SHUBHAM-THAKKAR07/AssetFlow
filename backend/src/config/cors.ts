871d9872928385a9723c986a160a2938a4049f71
Shubham Thakkar <134299058+SHUBHAM-THAKKAR07@users.noreply.github.com>
2026-07-26 15:10:09 +0530

    update cors file

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -2,10 +2,11 @@ import cors from 'cors';
 
 export const corsOptions: cors.CorsOptions = {
   origin: [
-  'https://shubham-thakkar07.github.io',
-  'http://localhost:5173',
-  'http://localhost:3000'
-],
+    'https://asset-flow-shubham07.vercel.app',
+    'https://shubham-thakkar07.github.io',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
